cdb08dd521250276e6fc7fc109dd4db018bf1c9e
elainemattje <elaine.mattje@valtech.com>
2020-02-05 11:14:38 -0300

    WIP- FAC-12- axios

---
+++ src/components/Axios_API.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Consulta CEP</h1>
+  <!-- <div class="consultaCEP"> -->
+  <!-- <input v-model="consultaCEP.cep" placeholder="Digite o cep" type="text" {{ cep }} /> -->
+  <!-- </div> -->
+</template>>
+<script>
+// import axios from "axios";
+export default {
+  data() {
+    return {
+      consultaCEP: {
+        cep: null
+      }
+    };
+  },
+  created: function() {
+    this.buscaCep();
+  },
+  methods: {
+    buscaCep: function() {
+      //   axios.get("http://").then(response => (this.cep = response));
+    }
+  }
+};
+</script>>
