df2e6a523361ff45ef4081b06927e9f7e379facd
elainemattje <elaine.mattje@valtech.com>
2020-02-13 16:36:24 -0300

    FAC-12-FE - create input

--- src/components/Axios_Dummy.vue
+++ src/components/Axios_Dummy.vue
@@ -19,6 +19,24 @@
         </tbody>
       </table>
     </div>
+    <div class="register">
+      <div class="regsiter--item">
+        <label class="name" for="name">Name</label>
+        <input class="input" type="text" name="name" v-model="inform.employee_name" />
+      </div>
+      <div class="regsiter--item">
+        <label class="name" for="age">Age</label>
+        <input class="input" type="text" name="age" v-model="inform.employee_age" />
+      </div>
+      <div class="regsiter--item">
+        <label class="name" for="salary">Salary</label>
+        <input class="input" type="text" name="salary" v-model="inform.employee_salary" />
+      </div>
+      <div class="regsiter--item">
+        <button class="btn" type="submit">Register</button>
+      </div>
+    </div>
+
     <transition name="fade">
       <div v-if="errored">Desculpe, não foi possível prosseguir com a sua solicitação.</div>
     </transition>
@@ -32,6 +50,12 @@ export default {
   name: "AxiosDummy",
   data() {
     return {
+      inform: {
+        id: "",
+        employee_name: "",
+        employee_age: "",
+        employee_salary: ""
+      },
       info: [],
       loading: true,
       errored: false
@@ -61,12 +85,10 @@ export default {
 };
 </script>>
 <style>
-.container {
-  width: 100%;
-  height: 100%;
-  background-color: rgb(122, 212, 235);
+h1 {
+  margin: 10px;
+  width: 800px;
   text-align: center;
-  padding: 10px;
 }
 
 .painting,
@@ -75,16 +97,44 @@ td {
   border: solid black 1px;
   box-sizing: border-box;
   border-collapse: collapse;
-  padding: 15px 15px;
+  padding: 10px 10px;
   width: 800px;
-  margin: 70px;
+  margin: 20px;
+  background-color: rgb(104, 196, 233);
 }
 
+th {
+  background-color: rgb(73, 185, 230);
+}
 th,
 td {
   width: 150px;
 }
 
+.register {
+  display: block;
+}
+
+.register--item {
+  text-align: left;
+}
+
+.name {
+  font-size: 20px;
+  margin: 10px 10px 10px 20px;
+}
+
+.input {
+  margin: 10px 0 0 50px;
+  width: 600px;
+  height: 20px;
+}
+
+.btn {
+  font-size: 15px;
+  padding: 10px;
+  margin: 10px 0 0 20px;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s, color 2s;
